01ccced6a9315c9b51ec577bc544ddedf35e0bec
Nelson <nelson@codics.am>
2019-08-09 17:56:16 +0400

    childrenscenters and artsakh without overall styling

--- js/main.js
+++ js/main.js
@@ -60,38 +60,101 @@ var artsakhSwiper = new Swiper('.artsakh-slider-1', {
 
 });
 
+var weSwiper = new Swiper('.we-banner-swiper', {
+    direction: 'horizontal',
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        bulletClass: "we-banner-swiper-pagination-bullet",
+        bulletActiveClass: "we-banner-swiper-pagination-bullet-active",
+    },
+    autoplay: {
+        delay: 3000,
+    },
+    disableOnInteraction: false,
+    effect: "fade",
+    speed: 3000,
+    fadeEffect: {
+        crossFade: true
+    },
+
+});
+
 
 // ##########################################
-//
-// $(document).ready(function () {
-//
-//
-//     var childrenscentersSlider = $('.childrenscenters-slider-2').slick({
-//
-//
-//         dots: true,
-//         arrows: false,
-//         autoplay: true,
-//         autoplaySpeed: 1000,
-//
-//         //     centerMode: true,
-//         //
-//         //     slidesToShow: 3,
-//         //     responsive: [
-//         //         {
-//         //             breakpoint: 768,
-//         //             settings:{
-//         //                 arrows: true,
-//         //                 centerMode: true,
-//         //                 centerPadding: '40px',
-//         //                 slidesToShow: 1
-//         //             }
-//         //         }
-//         //     ]
-//         //
-//         // })
-//
-//
-//     });
-// });
-//
+
+$(document).ready(function () {
+
+
+    var childrenscentersSlider = $('.childrenscenters-slider-2').slick({
+
+        dots: true,
+        // arrows: false,
+        // autoplay: true,
+        // autoplaySpeed: 1000,
+
+        centerMode: true,
+
+        slidesToShow: 1,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: true,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 1
+                }
+            }
+        ]
+
+    })
+
+
+    function changeSlideNext() {
+        childrenscentersSlider.slick('slickNext');
+    }
+
+    function changeSlidePrev() {
+        childrenscentersSlider.slick('slickPrev');
+    }
+
+    function killSlideChange(timer) {
+        window.clearTimeout(timer);
+    }
+
+
+    $(".childrenscentersSlider .slider-item").on('mouseover', function () {
+        var middle_index = $(".slick-center").attr('data-slick-index');
+        var current_index = $(this).attr('data-slick-index');
+        console.log('nnnn', middle_index - current_index);
+        if (middle_index - current_index > 0) {
+            console.log('prev');
+            changeSlidePrev();
+        } else {
+            console.log('prev');
+            changeSlideNext();
+
+        }
+
+    });
+
+    $(".childrenscentersSlider .slider-item").on('mouseout', function () {
+        var middle_index = $(".slick-center").attr('data-slick-index');
+        var current_index = $(this).attr('data-slick-index');
+        console.log('nnnn', middle_index - current_index);
+        if (middle_index - current_index > 0) {
+            console.log('prev');
+            changeSlidePrev();
+        } else {
+            console.log('prev');
+            changeSlideNext();
+
+        }
+
+    });
+
+
+});
+
