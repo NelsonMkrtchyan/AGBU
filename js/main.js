e156877665b9cb9e5321a9be63b1546931ecdfba
Nelson <nelson@codics.am>
2019-08-09 15:38:44 +0400

    childrenscenters and artsakh without overall styling

--- js/main.js
+++ js/main.js
@@ -1,14 +1,13 @@
-
-var opportunitySwiper = new Swiper ('.opportunity-slider-1', {
+var opportunitySwiper = new Swiper('.opportunity-slider-1', {
 
     direction: 'horizontal',
     loop: true,
     speed: 3000,
 
-    autoplay: { delay: 2500 },
+    autoplay: {delay: 2500},
     disableOnInteraction: false,
     effect: "fade",
-    fadeEffect: { crossFade: true },
+    fadeEffect: {crossFade: true},
 
     pagination: {
         el: '.swiper-pagination',
@@ -20,16 +19,16 @@ var opportunitySwiper = new Swiper ('.opportunity-slider-1', {
 });
 
 
-var childrenscentersSwiper = new Swiper ('.childrenscenters-slider-1', {
+var childrenscentersSwiper = new Swiper('.childrenscenters-slider-1', {
 
     direction: 'horizontal',
     loop: true,
     speed: 3000,
 
-    autoplay: { delay: 2500 },
+    autoplay: {delay: 2500},
     disableOnInteraction: false,
     effect: "fade",
-    fadeEffect: { crossFade: true },
+    fadeEffect: {crossFade: true},
 
     pagination: {
         el: '.swiper-pagination',
@@ -39,3 +38,60 @@ var childrenscentersSwiper = new Swiper ('.childrenscenters-slider-1', {
     },
 
 });
+
+
+var artsakhSwiper = new Swiper('.artsakh-slider-1', {
+
+    direction: 'horizontal',
+    loop: true,
+    speed: 3000,
+
+    autoplay: {delay: 2500},
+    disableOnInteraction: false,
+    effect: "fade",
+    fadeEffect: {crossFade: true},
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        // bulletClass: "opportunity-slider-1-pagination-bullet",
+        // bulletActiveClass: "opportunity-slider-1-pagination-bullet-active",
+    },
+
+});
+
+
+// ##########################################
+//
+// $(document).ready(function () {
+//
+//
+//     var childrenscentersSlider = $('.childrenscenters-slider-2').slick({
+//
+//
+//         dots: true,
+//         arrows: false,
+//         autoplay: true,
+//         autoplaySpeed: 1000,
+//
+//         //     centerMode: true,
+//         //
+//         //     slidesToShow: 3,
+//         //     responsive: [
+//         //         {
+//         //             breakpoint: 768,
+//         //             settings:{
+//         //                 arrows: true,
+//         //                 centerMode: true,
+//         //                 centerPadding: '40px',
+//         //                 slidesToShow: 1
+//         //             }
+//         //         }
+//         //     ]
+//         //
+//         // })
+//
+//
+//     });
+// });
+//
